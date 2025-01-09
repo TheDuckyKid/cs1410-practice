5b4843a7bf698e4f3b97ef2b60cc8274ec9c09f2
JackSquad765 <JackSquad765@gmail.com>
2025-01-09 03:48:08 -0700

    finished task 2 and 3

--- set1/numb2grade.py
+++ set1/numb2grade.py
@@ -1,4 +1,15 @@
 #make a grade calculator?
 print("Please type in a grade.")
 quacl = int(input())
-if
+grade = ""
+if quacl <= 59:
+    grade = "F"
+elif quacl <= 69:
+    grade = "D"
+elif quacl <= 79:
+    grade = "C"
+elif quacl <= 89:
+    grade = "B"
+elif quacl <= 100:
+    grade = "A"
+print("Your grade is " +grade)
--- set1/number3positivenegitivezero.py
+++ set1/number3positivenegitivezero.py
@@ -1,1 +1,11 @@
 #tell me if the number is + - or 0
+print("Please type in a number.")
+quacl = int(input())
+numP = ""
+if quacl >= 1:
+    numP = "Positive or +"
+elif quacl == 0:
+    numP = "Zero or 0"
+if quacl <= -1:
+    numP = "Negative or -"
+print("Your number is " +numP)
