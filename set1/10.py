4a904b831aa21bc7bb611ee5da798b8b3018c174
JackSquad765 <JackSquad765@gmail.com>
2025-01-16 22:50:19 -0700

    done with set 1 mostly have to finish any with TD

---
+++ set1/10.py
@@ -0,0 +1,7 @@
+#password checker
+passw = "Password123"
+userstr = input("What is the password?: ")
+if userstr == passw:
+    print("Access Granted")
+else:
+    print("Access Denied")
--- set1/9.py
+++ set1/9.py
@@ -3,5 +3,9 @@ Write a program that calculates a discount:
 
 10% off if the total is greater than $100
 Otherwise, no discount."""
-total = float(input(""))
-print(total)
+totalp = int(input("Please input your total: "))
+if totalp >= 100:
+    dtotal = totalp - (totalp *.10) 
+    print("Discount applied your new price is: $" + str(dtotal))
+else:
+    print("No Discount total is: $" + str(totalp))
